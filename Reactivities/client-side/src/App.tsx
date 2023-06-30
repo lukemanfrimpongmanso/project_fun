820d2f97ae1b60487510abbfbe2b6346e64bf804
Lukeman <lukemanfrimpongmanso360@gmail.com>
2023-06-30 16:53:38 +0200

    data fetch from api

--- Reactivities/client-side/src/App.tsx
+++ Reactivities/client-side/src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+    
+      <ul>
+        {activities.map((activity : any) => (
+          <li key={activity.id}>{activity.description}</li>
+        ))}
+      </ul>
     </div>
   );
 }
